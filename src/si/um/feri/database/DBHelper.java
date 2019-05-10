3910c69847cdbba12c8d37fdde1f5607cf046e77
Igor <igorstanisovski@gmail.com>
2019-05-10 09:35:02 +0200

    Koncna vaja 4.2

--- src/si/um/feri/database/DBHelper.java
+++ src/si/um/feri/database/DBHelper.java
@@ -10,7 +10,7 @@ public class DBHelper {
     private static BasicDataSource getDataSource() {
         if (dataSource == null) {
             BasicDataSource ds = new BasicDataSource();
-            ds.setUrl("jdbc:mysql://localhost:3306/sakila");
+            ds.setUrl("jdbc:mysql://localhost:3306/sakila?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
             ds.setUsername("root");
             ds.setPassword("igor123igor");
 
